c89619d1603e889aafd22920e5b45d084704b898
Momin <mahvash-nazir.momin@capgemini.com>
2019-04-11 16:16:52 +0530

    files added

---
+++ src/com/capgemini/lambda/client/CalculatorClient.java
@@ -0,0 +1,17 @@
+package com.capgemini.lambda.client;
+
+import com.capgemini.lambda.model.Calculator;
+
+public class CalculatorClient {
+
+	
+	public static void main(String[] args) {
+		Calculator addition = (x,y) -> x+y;
+		System.out.println(addition.calculate(30, 20));
+		
+		Calculator subtraction= (x,y) -> x-y;
+		System.out.println(subtraction.calculate(30,20));
+	}
+}
+
+
